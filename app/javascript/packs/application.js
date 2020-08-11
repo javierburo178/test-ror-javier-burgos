3b702c0144a68cb1c1297a1c957d9f871e7979bb
javierburo178 <andres178bur@gmail.com>
2020-08-11 17:21:46 -0500

    Fixed Jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,10 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import 'bootstrap'
+import '../stylesheets/application'
 
+require("jquery")
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -16,8 +19,9 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
 require("trix")
 require("@rails/actiontext")
+
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toggle="tooltip"]').tooltip()
+})
