5dcbf692a55ac451b353c50f581f82697b4bc296
javierburo178 <andres178bur@gmail.com>
2020-08-12 18:09:37 -0500

    Test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @post = posts(:default)
+  end
+
+  test 'should validate length of title' do
+    assert_must validate_length_of(:title), @post
+  end
+
+  test 'should validate presence of title' do
+    assert_must validate_presence_of(:title), @post
+  end
+
+  test 'should validate presence of description' do
+    assert_must validate_presence_of(:description), @post
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
+require File.expand_path('../config/environment', __dir__)
 require 'rails/test_help'
+require 'shoulda/matchers'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
@@ -10,4 +11,10 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :minitest
+      with.library :rails
+    end
+  end
 end
